3398585f1f3f754f318aed77580bfdc3da420ae6
Erin <juries.erin@gmail.com>
2024-03-23 22:48:43 +0100

    Built Tailwind test config styling

--- client-side/my-app/tailwind.config.js
+++ client-side/my-app/tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '0.25rem',
+      DEFAULT: '4px',
+      'md': '0.375rem',
+      'lg': '0.5rem',
+      'full': '9999px',
+      'large': '30px',
+    },
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+        'text-color': '#192A51',
+        'Mountbatten-pink': '#967AA1',
+        'Rose-quartz': '#aaa1c8',
+        'Thistle': '#D5C6E0',
+        'Lavender-blush': "#F5E6E8",
+        'dark': '#333333',
+        'Misty-rose': '#F1DEDE',
+      },
+      backgroundImage: {
+        'login': "url('./src/assets/login-img.jpg')",
+      }
+    },
   },
   plugins: [],
 }
