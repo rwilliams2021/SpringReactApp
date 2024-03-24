1d0cfc7c09a463158988e36d658a5629ea1fb809
rwilliams <richyrichw93@gmail.com>
2024-03-24 21:27:05 +0100

    make fields unique and required

--- backend/src/main/java/com/springreact/models/User.java
+++ backend/src/main/java/com/springreact/models/User.java
@@ -1,7 +1,10 @@
 package com.springreact.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.math.BigInteger;
 
 @MappedSuperclass
 @Data
@@ -9,7 +12,12 @@ public abstract class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull(message = "Field 'idNumber' is required")
+    @Column(unique = true)
+    private BigInteger idNumber;
+    @NotNull(message = "Field 'firstName' is required")
     String firstName;
+    @NotNull(message = "Field 'lastName' is required")
     String lastName;
     int dob;
     String phoneNumber;
